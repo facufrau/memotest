f7411fd6991372f341794d366f36ba60cf2a9531
Facundo Frau <facufrau@gmail.com>
2021-07-27 17:56:41 -0300

    Reorganiza funcion mezclarCartas
    Debido a que las cartas se decidieron a guardar en un array de objetos se elimina la funcion mezclarIndices innecesaria y se realiza todo en la funcion mezclarCartas

--- index.js
+++ index.js
@@ -1,36 +1,24 @@
 const $botonJugar = document.querySelector("#jugar");
 const $botonResetear = document.querySelector("#reiniciar");
 const CANTIDAD_CARTAS = 20;
-let arrayDeCartas = [];
+const $cantidadIntentos = document.querySelector("#intentos");
+const $tiempo = document.querySelector("#tiempo");
 
 $botonJugar.onclick = jugar;
 $botonResetear.onclick = resetear;
 
 function jugar() {
-    let indicesCartasMezclados = mezclarNumerosCartas();
-    cargarCartasEnObjeto(arrayDeCartas);
-    //console.log(arrayDeCartas);
-    mostrarCartas(indicesCartasMezclados);
+    arrayDeCartas = [];
+    cargarCartasEnArray(arrayDeCartas);
+    mezclarCartas(arrayDeCartas);
+    mostrarCartas(arrayDeCartas);
 }
 
 function resetear() {
     ocultarCartas();
 }
 
-function mezclarNumerosCartas() {
-    // Idea para esta función tomada de: https://stackoverflow.com/a/12646864
-    // Generar una lista con numeros consecutivos https://stackoverflow.com/a/33352604 
-    let listaNumeros = [...Array(CANTIDAD_CARTAS).keys()];
-    for (let i = listaNumeros.length - 1; i > 0; i--) {
-        let indice = Math.floor(Math.random() * (i + 1));
-        let temporal = listaNumeros[i];
-        listaNumeros[i] = listaNumeros[indice];
-        listaNumeros[indice] = temporal;
-    }
-    return listaNumeros
-}
-
-function cargarCartasEnObjeto(arrayDeCartas) {
+function cargarCartasEnArray(arrayDeCartas) {
     for (let i = 0; i < CANTIDAD_CARTAS / 2; i++ ){
         let cartaOperacion = {"tipo": i, "ruta": `imagenes/operaciones/${i}.png`};
         arrayDeCartas.push(cartaOperacion);
@@ -40,13 +28,13 @@ function cargarCartasEnObjeto(arrayDeCartas) {
     }
 }
 
-
-function mostrarCartas(listaIndices) {
-    let indice;
-    for (let i = 0; i < listaIndices.length; i++) {
-        let carta = document.querySelector(`#carta-${i}`);
-        indice = listaIndices[i] 
-        carta.src = arrayDeCartas[indice]["ruta"];
+function mezclarCartas(arrayDeCartas) {
+    // Idea para esta función tomada de: https://stackoverflow.com/a/12646864
+    for (let i = CANTIDAD_CARTAS - 1; i > 0; i--) {
+        let indice = Math.floor(Math.random() * (i + 1));
+        let temporal = arrayDeCartas[i];
+        arrayDeCartas[i] = arrayDeCartas[indice];
+        arrayDeCartas[indice] = temporal;
     }
 }
 
