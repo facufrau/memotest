7e85da44b3c7f3b6294adf61c33ffca5f36224e4
Facundo Frau <facufrau@gmail.com>
2021-07-26 21:02:52 -0300

    Agrega funcion cargar cartas, constantes y crea objeto para cartas
    Se agrega una función que cargue las cartas en un objeto debido a que se decide que las cartas que forman el par no sean iguales sino una operación matemática y su resultado. Se crean constantes para utilizar en bucles for en lugar de usar lista de valores harcodeados

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const $botonJugar = document.querySelector("#jugar");
 const $botonResetear = document.querySelector("#reiniciar");
+const CANTIDAD_CARTAS = 20;
+let arrayDeCartas = [];
+
 $botonJugar.onclick = jugar;
 $botonResetear.onclick = resetear;
 
 function jugar() {
     let indicesCartasMezclados = mezclarNumerosCartas();
+    cargarCartasEnObjeto(arrayDeCartas);
+    //console.log(arrayDeCartas);
     mostrarCartas(indicesCartasMezclados);
 }
 
@@ -14,7 +19,8 @@ function resetear() {
 
 function mezclarNumerosCartas() {
     // Idea para esta función tomada de: https://stackoverflow.com/a/12646864
-    let listaNumeros = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+    // Generar una lista con numeros consecutivos https://stackoverflow.com/a/33352604 
+    let listaNumeros = [...Array(CANTIDAD_CARTAS).keys()];
     for (let i = listaNumeros.length - 1; i > 0; i--) {
         let indice = Math.floor(Math.random() * (i + 1));
         let temporal = listaNumeros[i];
@@ -24,15 +30,28 @@ function mezclarNumerosCartas() {
     return listaNumeros
 }
 
+function cargarCartasEnObjeto(arrayDeCartas) {
+    for (let i = 0; i < CANTIDAD_CARTAS / 2; i++ ){
+        let cartaOperacion = {"tipo": i, "ruta": `imagenes/operaciones/${i}.png`};
+        arrayDeCartas.push(cartaOperacion);
+
+        let cartaRespuesta = {"tipo": i, "ruta": `imagenes/respuestas/${i}.png`};
+        arrayDeCartas.push(cartaRespuesta);
+    }
+}
+
+
 function mostrarCartas(listaIndices) {
+    let indice;
     for (let i = 0; i < listaIndices.length; i++) {
         let carta = document.querySelector(`#carta-${i}`);
-        carta.src = `imagenes/${listaIndices[i]}.png`;
+        indice = listaIndices[i] 
+        carta.src = arrayDeCartas[indice]["ruta"];
     }
 }
 
 function ocultarCartas() {
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < CANTIDAD_CARTAS; i++) {
         let carta = document.querySelector(`#carta-${i}`);
         carta.src = "imagenes/reverso.png";
     }
