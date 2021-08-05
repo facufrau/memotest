0a789e473fc3e8567982889e3610d2a305ea91fe
Facundo Frau <facufrau@gmail.com>
2021-08-04 23:45:16 -0300

    Mejora legibilidad del array de cartas.

--- src/cartas.js
+++ src/cartas.js
@@ -1,83 +1,23 @@
 const cartas = [
-    {
-      "id": 1,
-      "ruta": "imagenes/1.png"
-    },
-    {
-      "id": 2,
-      "ruta": "imagenes/2.png"
-    },
-    {
-      "id": 3,
-      "ruta": "imagenes/3.png"
-    },
-    {
-      "id": 4,
-      "ruta": "imagenes/4.png"
-    },
-    {
-      "id": 5,
-      "ruta": "imagenes/5.png"
-    },
-    {
-      "id": 6,
-      "ruta": "imagenes/6.png"
-    },
-    {
-      "id": 7,
-      "ruta": "imagenes/7.png"
-    },
-    {
-      "id": 8,
-      "ruta": "imagenes/8.png"
-    },
-    {
-      "id": 9,
-      "ruta": "imagenes/9.png"
-    },
-    {
-      "id": 10,
-      "ruta": "imagenes/10.png"
-    },
-    {
-      "id": 1,
-      "ruta": "imagenes/1.png"
-    },
-    {
-      "id": 2,
-      "ruta": "imagenes/2.png"
-    },
-    {
-      "id": 3,
-      "ruta": "imagenes/3.png"
-    },
-    {
-      "id": 4,
-      "ruta": "imagenes/4.png"
-    },
-    {
-      "id": 5,
-      "ruta": "imagenes/5.png"
-    },
-    {
-      "id": 6,
-      "ruta": "imagenes/6.png"
-    },
-    {
-      "id": 7,
-      "ruta": "imagenes/7.png"
-    },
-    {
-      "id": 8,
-      "ruta": "imagenes/8.png"
-    },
-    {
-      "id": 9,
-      "ruta": "imagenes/9.png"
-    },
-    {
-      "id": 10,
-      "ruta": "imagenes/10.png"
-    }
+    {"id": 1,"ruta": "imagenes/1.png"},
+    {"id": 2,"ruta": "imagenes/2.png"},
+    {"id": 3,"ruta": "imagenes/3.png"},
+    {"id": 4,"ruta": "imagenes/4.png"},
+    {"id": 5,"ruta": "imagenes/5.png"},
+    {"id": 6,"ruta": "imagenes/6.png"},
+    {"id": 7,"ruta": "imagenes/7.png"},
+    {"id": 8,"ruta": "imagenes/8.png"},
+    {"id": 9,"ruta": "imagenes/9.png"},
+    {"id": 10,"ruta": "imagenes/10.png"},
+    {"id": 1,"ruta": "imagenes/1.png"},
+    {"id": 2,"ruta": "imagenes/2.png"},
+    {"id": 3,"ruta": "imagenes/3.png"},
+    {"id": 4,"ruta": "imagenes/4.png"},
+    {"id": 5,"ruta": "imagenes/5.png"},
+    {"id": 6,"ruta": "imagenes/6.png"},
+    {"id": 7,"ruta": "imagenes/7.png"},
+    {"id": 8,"ruta": "imagenes/8.png"},
+    {"id": 9,"ruta": "imagenes/9.png"},
+    {"id": 10,"ruta": "imagenes/10.png"}
   ]
   
